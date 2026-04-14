c6ed0907626d4883de17a2c7c77020c9e81cb817
kyanagis <kyanagis42@gmail.com>
2026-04-15 03:56:18 +0900

    ci(github): add nix validation workflow

--- bootstrap/linux.sh
+++ bootstrap/linux.sh
@@ -18,7 +18,7 @@ if ! command -v nix >/dev/null 2>&1; then
 fi
 
 if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
-  # shellcheck disable=SC1090
+  # shellcheck disable=SC1090,SC1091
   . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 fi
 
